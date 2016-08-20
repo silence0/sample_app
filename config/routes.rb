fb796d0a640b46f140c0ee5e6160fed718a82e69
djc <djc@djcdeMacBook-Air.local>
2016-08-20 12:58:55 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'signup' => 'user#new'
+  get 'signup' => 'users#new'
 
   get 'help' => 'static_pages#help'
 
@@ -20,8 +20,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
+  resources :users
   # Example resource route with options:
   #   resources :products do
   #     member do
