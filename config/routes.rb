35f48c04de0f8098fe9b0950d4c8b440cf9a960f
djc <djc@djcdeMacBook-Air.local>
2016-08-21 16:36:46 +0800

    Finish login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create' 
+  
+  delete 'logout' => 'sessions#destroy'
+
   get 'signup' => 'users#new'
 
   get 'help' => 'static_pages#help'
